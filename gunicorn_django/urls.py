04f1e57f56038077ce7d0b3d9848e634156b4434
zaseka.bogdan <zaseka.bogdan@gmai.com>
2018-03-12 00:05:56 +0200

    change router

--- gunicorn_django/urls.py
+++ gunicorn_django/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^app/', include('app.urls')),
+    url(r'^', include('app.urls')),
 ]
