bc4532646d34e33732b022fbad8a52fab82ab795
zaseka.bogdan <zaseka.bogdan@gmai.com>
2018-03-14 23:35:44 +0200

    add class for saving hash password from admin side

--- app/admin.py
+++ app/admin.py
@@ -3,10 +3,19 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import Answer, Question, User
+import hashlib
 
 # Register your models here.
 
 admin.site.register(Answer)
 admin.site.register(Question)
-admin.site.register(User)
 
+
+class UserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        password = form.cleaned_data['password']
+        obj.password = hashlib.md5(password.encode()).hexdigest()
+        obj.save()
+
+
+admin.site.register(User, UserAdmin)
